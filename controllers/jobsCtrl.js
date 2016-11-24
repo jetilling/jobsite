d71f8869d0cb659855ff673930421fceb7138f28
jetilling <jetilling@hotmail.com>
2016-11-24 11:17:38 -0700

    get userId needs workd

--- controllers/jobsCtrl.js
+++ controllers/jobsCtrl.js
@@ -3,6 +3,13 @@ var app = require('../server'),
     config = require('../config.json');
 
 module.exports = {
+  userData: function(req, res){
+    db.get_user_data([req.params.id], function(err, users){
+      if(err) console.log(err);
+      else res.status(200).send(users);
+    })
+  },
+
   createJob: function(req, res){
     db.create_job([req.body.userId, req.body.title, req.body.description, req.body.bid, req.body.keyword, req.body.hours], function(err, jobs){
       if(err) console.log(err);
