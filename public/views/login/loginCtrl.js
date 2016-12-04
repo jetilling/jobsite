b54bbd076f8de944914a7e53bfa622ae16c59727
jetilling <jetilling@hotmail.com>
2016-12-03 17:08:59 -0700

    retrieves posts on login - need a way to keep them

--- public/views/login/loginCtrl.js
+++ public/views/login/loginCtrl.js
@@ -8,9 +8,10 @@ angular.module('jobSite').controller("loginCtrl", function($scope, $auth, $state
       password: password,
     }).then(function (response) {
       console.log("loginCtrl:", response);
-      mainService.userData = response.data.user
+      // mainService.userData = response.data.user
       if(response.status === 200){
-        $auth.setToken(response)
+        $auth.setToken(response);
+        mainService.userData();
         $state.go('profile');
       }
     }).catch(function (response) {
